739baf67ba14e5559c6e8601d405712a6cc8b9cf
Irfat <imirfat10@gmail.com>
2024-06-04 12:37:46 +0600

    nav z index changed

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     }
 
     return (
-        <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-base-100">
+        <div className="navbar fixed z-50 bg-opacity-30 max-w-screen-xl bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
